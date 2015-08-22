e802134c6e2c9bb3739db3c95c97b6cfeaad86b8
VictorCoding <vramos@iofficecorp.com>
2015-08-22 17:33:05 -0500

    We can add file paths now Add css stylesheet for our project Add textarea for the text we want to write to all files Add function to check if file exists before adding to list of file to write to Add function to write to all files in list(no code in body yet)

--- app/app.js
+++ app/app.js
@@ -5,12 +5,38 @@
         .module('FileSync', [])
         .controller('MainCtrl', MainCtrl);
             
-  function MainCtrl(FileManager){
-    var mc = this;
+  function MainCtrl(FileManager, $timeout){
+    var mc = this,
+        fm = FileManager;
+        
+    mc.checkedFile = false;
+    mc.dupFileError = false;
     mc.FilesToWriteTo = [];    
     
-    mc.addFile = function addFile(){      
-      mc.FileExists = FileManager.exists(mc.FileName);
+    mc.addFile = function addFile(file){   
+      if(mc.FilesToWriteTo.indexOf(file) > -1){
+        mc.dupFileError = true;
+        $timeout(function(){
+          mc.dupFileError = false;
+        }, 2000);
+        return;
+      }
+      mc.FileExists = fm.exists(file);
+      mc.checkedFile = true;
+      
+      if(mc.FileExists){
+        mc.FilesToWriteTo.push(file);
+      }
+      
+    };
+    
+    
+    mc.writeToAll = function writeToAll(text){
+      if(mc.FilesToWriteTo.length > 0){
+        angular.forEach(mc.FilesToWriteTo, function(file){
+          console.log(file, text);
+        });
+      }
     };
     
   }
