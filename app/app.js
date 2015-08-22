4ee5b19a3b5d346b0b47ac17755cc1b33fa2ad1b
VictorCoding <vramos@iofficecorp.com>
2015-08-22 13:09:42 -0500

    Adding angularjs 1.4 and initial angular module/controller setup

---
+++ app/app.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+  
+  angular
+        .module('FileSync', [])
+        .controller('MainCtrl', MainCtrl);
+        
+  function MainCtrl(){
+    var vm = this;
+  }
+})();
