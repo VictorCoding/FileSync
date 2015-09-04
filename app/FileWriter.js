03f137c7b9aa237fa45e763e094de60cbe18d8ec
VictorCoding <vramos@iofficecorp.com>
2015-09-04 10:21:50 -0500

    Add check for json for special format of quotes and colon

--- app/FileWriter.js
+++ app/FileWriter.js
@@ -16,8 +16,18 @@ function FileManager(){
     return IsThere(name);
   }
   
-  function write(fileName, newText){
-    if(newText){
+  function write(fileName, newText){    
+    var fileExtension = fileName.split('.')[1];
+    
+    if(newText && fileName){            
+      if(fileExtension === 'json'){        
+        //replace the equal sign(=) with quotes and a colon(": ")
+        newText = newText.replace(/=/gi, '": "');
+        //wrap everything in double quotes because json
+        //and two tabs because code styles
+        //ex: "this_is_a_key": "this is a key"
+        newText = '\n\t\t"' + newText + '",';
+      }      
       newText = '\n' + newText;
     }
     fs.appendFile(fileName, newText);
