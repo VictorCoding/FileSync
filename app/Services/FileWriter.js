8bde610eb3879b5369c04ca3b857fe46cb83834a
VictorCoding <vramos@iofficecorp.com>
2015-09-28 11:15:47 -0500

    Added logic to prepend 2 exclamation marks to key values when file is not one of the default, us, or uk.

--- app/Services/FileWriter.js
+++ app/Services/FileWriter.js
@@ -19,6 +19,15 @@ function FileManager(){
   function write(fileName, newText){   
     var fileContent = fs.readFileSync(fileName, 'utf8');       
     var fileExtension = fileName.split('.')[1];
+    var defaultsFiles = ['en_US', 'default', 'en_GB', 'iOfficeConnect', 'iOfficeConnect_en_US',
+                        'iOfficeConnect_en_GB'];
+                        //Crazy chain!
+                        // 1. Split filename by the period. Then grab the first item, which
+                        //    will be the file path without the extension.
+                        // 2. Then split it by the '/' to seperate the directories names
+                        // 3. You figure out the last index of '/' and splice that mofo right there,
+                        //    which will return you the last name being the actual localization file name.
+    var locale =  fileName.split('.')[0].split('/').splice(_.lastIndexOf(fileName.split('.')[0].split('/'), '/'))[0];    
     var pos;
     
     fileContent = fileContent.split('\n');
@@ -27,15 +36,23 @@ function FileManager(){
     if(newText && fileName){            
       if(fileExtension === 'json'){        
         //replace the equal sign(=) with quotes and a colon(": ")
-        newText = newText.replace(/=/gi, '": "');
+        if(defaultsFiles.indexOf(locale) < 0){
+          newText = newText.replace(/=/gi, '": "!!');
+        } else {
+          newText = newText.replace(/=/gi, '": "');
+        }
         //wrap everything in double quotes because json
         //and two tabs because code styles
         //ex: "this_is_a_key": "this is a key"
-        newText = '\t\t"' + newText + '"';
+        newText = '  "' + newText + '"';                
         
         pos = pos - 1;
         var lastItem = fileContent[pos - 1] + ',';
         fileContent.splice(pos - 1, 1, lastItem);
+      } else {
+        if(defaultsFiles.indexOf(locale) < 0){
+          newText = newText.replace(/=/gi, '=!!');
+        }
       }      
                               
       fileContent.splice(pos, 0, newText);
